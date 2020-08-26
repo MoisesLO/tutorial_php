801f6538872d9ff9ad0a9fa85f877101289185a4
moises <moises@moisess-iMac.local>
2020-08-26 12:32:59 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/lineysoft/php/tutorial_php/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lineysoft.php.tutorial_php
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Tutorial PHP'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(widget.title),
+      ),
+      body: ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Map'),
+              )
+            ],
+          )
+    );
+  }
+}
